561db74ec9cd4f7cfa9ea6364c7a13052b709017
Tisavaco <40355964+Tisavaco@users.noreply.github.com>
2023-12-06 09:52:06 +0300

    Add files via upload

---
+++ OPC UA MVP/OPCClient/OPCClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Opc.UaFx.Client;
+
+
+
+const string sinTag = "Sinx";
+
+Uri serverUrl = new Uri("opc.tcp://localhost:62541/Quickstarts/ReferenceServer");
+var client = new OpcClient(serverUrl);
+
+
+Console.WriteLine("Подключение к серверу...");
+client.Connect();
+Console.WriteLine("Connected!");
+
+var subscription = client.SubscribeNode("ns=2;s=" + sinTag);
+
+subscription.DataChangeReceived += (sender, e) =>
+{
+
+    var data = client.ReadNode("ns=2;s=" + sinTag);
+    Console.WriteLine(sinTag +": "+ data.ToString());
+};
+
+
+Console.ReadKey();
+
+
+client.Disconnect();
---
+++ OPC UA MVP/OPCServer/OPCServer/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Opc.Ua;
+using Opc.Ua.Configuration;
+using OPCServer;
+
+ApplicationInstance application = new ApplicationInstance();
+application.ApplicationType = ApplicationType.Server;
+application.ConfigSectionName = "Quickstarts.ReferenceServer";
+var server = new ReferenceServer();
+await application.Start(server);
+Console.WriteLine("Сервер запущен!");
+Console.ReadKey();
+application.Stop();
+
+
